8780402c57bb9b43069d140622a4b3d97f027ed2
adrianoadacosta <adrianoadacosta@outlook.com>
2024-03-05 20:35:07 -0300

    pagina inicial + links demais paginas+ navegação entre elas

---
+++ JS/index.js
@@ -0,0 +1,16 @@
+/** @format */
+
+document.getElementById('caca-palavras').addEventListener('click', function () {
+	// Redireciona para outra página
+	window.location.href = 'pages/cacaPalavra.html';
+});
+
+document.getElementById('jogo-memoria').addEventListener('click', function () {
+	// Redireciona para outra página
+	window.location.href = 'pages/memoria.html';
+});
+
+document.getElementById('Quizz').addEventListener('click', function () {
+	// Redireciona para outra página
+	window.location.href = 'pages/quiz.html';
+});
